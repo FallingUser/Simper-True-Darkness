6dc2fcad0e1aa7ee78f1fe08022ca1cc2684d8dd
FallingUser <1265336243@qq.com>
2026-07-20 16:10:27 +0800

    Update MixinEndEffects.java

--- src/main/java/grondag/darkness/mixin/MixinEndEffects.java
+++ src/main/java/grondag/darkness/mixin/MixinEndEffects.java
@@ -20,19 +20,19 @@
 
 package grondag.darkness.mixin;
 
+import grondag.darkness.Darkness;
+import net.minecraft.client.renderer.DimensionSpecialEffects;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.client.renderer.DimensionSpecialEffects;
-import net.minecraft.world.phys.Vec3;
-
-import grondag.darkness.Darkness;
-
 @Mixin(DimensionSpecialEffects.EndEffects.class)
 public class MixinEndEffects {
-	private static double MIN = 0.029999999329447746D;
+	@Unique
+    private static final double MIN = 0.029999999329447746D;
 
 	@Inject(method = "getBrightnessDependentFogColor", at = @At(value = "RETURN"), cancellable = true)
 	private void onAdjustFogColor(CallbackInfoReturnable<Vec3> ci) {
@@ -41,7 +41,6 @@ public class MixinEndEffects {
 		if (factor != 1.0) {
 			Vec3 result = ci.getReturnValue();
 			result = new Vec3(Math.max(MIN, result.x * factor), Math.max(MIN, result.y * factor), Math.max(MIN, result.z * factor));
-
 			ci.setReturnValue(result);
 		}
 	}
